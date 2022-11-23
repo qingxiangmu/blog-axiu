c9f153b1a68a6d73d5e65d0525a2f9990956cffd
xuanyan <1430392553@qq.com>
2022-11-23 10:31:57 +0800

    更新go-blog 数据

--- main.go
+++ main.go
@@ -1,37 +1,16 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
-	"os"
+	"ms-go-blog/common"
+	"ms-go-blog/server"
 )
 
-type IndexData struct {
-	Title string `json:"title"`
-	Desc  string `json:"desc"`
+func init() {
+	//模板加载
+	common.LoadTemplate()
 }
-
-func index(w http.ResponseWriter, r *http.Request) {
-	var indexData IndexData
-	indexData.Title = "axiu's blog"
-	indexData.Desc = "now is introduction"
-	t := template.New("index.html")
-	path, _ := os.Getwd()
-	home := path + "/template/home.html"
-	header := path + "/template/home.html"
-	footer := path + "/template/layout/footer.html"
-	personal := path + "/template/layout/personal.html"
-	post := path + "/template/layout/post-list.html"
-	pagination := path + "/template/layout/pagination.html"
-	t, _ = t.ParseFiles(path+"/template/index.html", header, home, footer, personal, post, pagination)
-	t.Execute(w, indexData)
-}
-
 func main() {
-	server := http.Server{
-		Addr: "127.0.0.1:9898",
-	}
-	http.HandleFunc("/", index)
-	if err := server.ListenAndServe(); err != nil {
-	}
+	//程序入口，一个项目 只能有一个入口
+	//web程序，http协议 ip port
+	server.App.Start("127.0.0.1", "8080")
 }
